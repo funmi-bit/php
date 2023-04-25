ebffff42ccf3abbf35289dc191df316168c1cd43
funmi <funmilayo_hamed@yahoo.com>
2023-04-25 10:13:25 +0100

    Add files via upload

---
+++ list.py
@@ -0,0 +1,19 @@
+#num_1 = [1, 2, 3, 4, 5,]
+#num_2 = [20, 30 ,40, 50, 60]
+
+#def adder():
+#    num_3 = []
+#    for i in range(len(num_1)):
+ #       value = num_1[i] + num_2[i]
+#        num_3.append(value)
+ #   return num_3
+
+#num_3 = adder()
+#print(num_3)
+
+
+peers = ["yesus"," wolid", "celisha"]
+def names():
+     for name in peers:
+         print("hello",  name)
+names()
